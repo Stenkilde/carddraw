2ca4b3aa39366a9b4f01e29ca34009b72d251432
Stenkilde <hello@stenkil.de>
2015-12-02 10:42:22 +0100

    Worked on card back and added new gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
  
 gulp.task('serve', function() {
   connect.server();
 });
+
+gulp.task('sass', function () {
+  gulp.src('./styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles'));
+});
  
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
 gulp.task('default', ['serve']);
